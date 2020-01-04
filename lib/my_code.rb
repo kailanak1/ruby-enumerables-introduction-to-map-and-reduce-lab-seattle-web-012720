69039ce8267ebed8caceb58645dc4a6fc153cc97
Kailana Kahawaii <kailanakahawaii@gmail.com>
2020-01-04 20:09:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,14 +58,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do 
-    if source_array[i] = true 
-      return true 
-    if source_array[i] = false 
-      return false 
-    end 
-    end 
-    i += 1 
-  end 
+ 
 end 
 
 
